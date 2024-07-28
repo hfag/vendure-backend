9547408be6e71096fab41f3860e36e7cfae7e936
Nico Hauser <me@tyratox.ch>
2024-07-28 11:57:43 +0200

    Update shipping cost calculator

--- src/plugins/shipping/shipping.ts
+++ src/plugins/shipping/shipping.ts
@@ -1,5 +1,10 @@
 import { LanguageCode } from "@vendure/common/lib/generated-types";
-import { RequestContext, ShippingCalculator } from "@vendure/core";
+import {
+  ProductService,
+  ProductVariantService,
+  RequestContext,
+  ShippingCalculator,
+} from "@vendure/core";
 
 enum TaxSetting {
   include = "include",
@@ -8,6 +13,10 @@ enum TaxSetting {
 }
 
 export const flatShippingCalculator = new ShippingCalculator({
+  init: function (injector) {
+    (this as any).productService = injector.get(ProductService);
+    (this as any).productVariantService = injector.get(ProductVariantService);
+  },
   code: "flat-shipping-calculator",
   description: [
     {
@@ -20,7 +29,7 @@ export const flatShippingCalculator = new ShippingCalculator({
     },
   ],
   args: {
-    rate: {
+    defaultRate: {
       type: "int",
       defaultValue: 0,
       ui: { component: "currency-form-input" },
@@ -81,10 +90,87 @@ export const flatShippingCalculator = new ShippingCalculator({
         { languageCode: LanguageCode.de, value: "Steursatz" },
       ],
     },
+    facets: {
+      type: "ID",
+      list: true,
+      ui: { component: "facet-value-form-input" },
+    },
+    facetRates: {
+      type: "int",
+      defaultValue: 0,
+      list: true,
+      ui: { component: "currency-form-input" },
+      label: [
+        { languageCode: LanguageCode.en, value: "Shipping price" },
+        { languageCode: LanguageCode.de, value: "Lieferkosten" },
+      ],
+    },
   },
-  calculate: (ctx, order, args) => {
+  calculate: async function (ctx, order, args) {
+    let price = args.defaultRate;
+
+    const productService = (this as any).options
+      .productService as ProductService;
+
+    const productVaraintService = (this as any).options
+      .productVariantService as ProductVariantService;
+
+    const products = await productService.findByIds(
+      ctx,
+      order.lines.map((line) => line.productVariant.productId)
+    );
+
+    const variants = await productVaraintService.findByIds(
+      ctx,
+      order.lines.map((line) => line.productVariant.id)
+    );
+
+    if (args.facets.length === args.facetRates.length) {
+      price = order.lines.reduce((maxPrice, line) => {
+        const product = products.find(
+          (p) => p.id == line.productVariant.productId
+        );
+
+        if (!product) {
+          console.log(products);
+          throw new Error(
+            `This should not be able to happen, we loaded this product before (id = ${line.productVariant.productId})`
+          );
+        }
+
+        const variant = variants.find((p) => p.id == line.productVariant.id);
+
+        if (!variant) {
+          throw new Error(
+            `This should not be able to happen, we loaded this variant before (id = ${line.productVariant.id})`
+          );
+        }
+
+        const facetValueIds = [
+          ...product.facetValues,
+          ...variant.facetValues,
+        ].map((v) => v.id);
+
+        const maxPriceOverFacets = args.facets.reduce<number>(
+          (maxPrice, facetId) => {
+            const idx = facetValueIds.findIndex((id) => id == facetId);
+            if (idx < 0) {
+              return maxPrice;
+            }
+
+            const priceForFacet = args.facetRates[idx];
+
+            return priceForFacet > maxPrice ? priceForFacet : maxPrice;
+          },
+          args.defaultRate
+        );
+
+        return maxPriceOverFacets > maxPrice ? maxPriceOverFacets : maxPrice;
+      }, price);
+    }
+
     return {
-      price: args.rate,
+      price: price,
       taxRate: args.taxRate,
       priceIncludesTax: getPriceIncludesTax(
         ctx,
